7cd2ddb4664711104dd8c6389038b2982fd4d356
sebastien baiges-bello <baigesbellosebastien@gmail.com>
2019-08-07 17:15:32 +0200

    "update"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Creating restaurants...'
+category_choice = %w(chinese italian japanese french belgian)
+restaurants_attributes = [
+
+  {
+    name:         "Dishoom",
+    address:      "7 Boundary St, London E2 7JE",
+    phone_number:  "0618225584",
+    category:       category_choice.shuffle.first
+  },
+
+  {
+    name:         "Jiji",
+    address:      "Carotte, Birmingham E2 7JE",
+    phone_number:  "0618349876",
+    category:       category_choice.shuffle.first
+  },
+
+  {
+    name:         "Toto",
+    address:      "Jp, Oxford E2 33E",
+    phone_number:  "0633878233",
+    category:      category_choice.shuffle.first
+  },
+
+  {
+    name:         "Vava",
+    address:      "Peyrehorade, 40300",
+    phone_number:  "0623873233",
+    category:      category_choice.shuffle.first
+  },
+
+  {
+    name:         "Lolo",
+    address:      "Ferrari, 87330",
+    phone_number:  "0633893234",
+    category:      category_choice.shuffle.first
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
